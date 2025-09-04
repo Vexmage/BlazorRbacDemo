5fa88a97ed93a837d7a85386a8476763089b0d1f
Joel Southall <vextechmage@gmail.com>
2025-09-04 05:11:52 -0700

    debug and fix FK error with deleted orders

---
+++ Migrations/20250904120759_AuditLog_SetNullOnDelete.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorRbacDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class AuditLog_SetNullOnDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
